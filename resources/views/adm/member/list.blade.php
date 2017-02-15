286f791a9728db712250124f4f4751d2f19f5362
test <test@test0047.axcell28.idv.tw>
2017-02-15 08:03:04 +0800

    member, news, page list html and css finish, news create html and css finish

--- resources/views/adm/member/list.blade.php
+++ resources/views/adm/member/list.blade.php
@@ -5,9 +5,76 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href='/lib/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet' />
         <link href='/lib/bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet' />
+        <link href='/css/admin/header.css' rel='stylesheet' />
+        <link href='/css/admin/dashboard-left.css' rel='stylesheet' />
+        <link href='/css/admin/dashboard.css' rel='stylesheet' />
     </head>
     <body>
+        @include('adm.header')
         @include('adm.dashboard-left')
+        <span class="breadcrumbs col-xs-9">
+            會員列表
+            <a href="#">+</a>
+        </span>
+        <div class="col-xs-9 memList">
+            <table class="col-xs-12 table">
+                <thead>
+                    <tr>
+                        <td>會員名稱</td>
+                        <td>email</td>
+                        <td>建立時間</td>
+                        <td>操作</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>陳小明</td>
+                        <td>test8@test.com.tw</td>
+                        <td>2016-12-03 12:14:28</td>
+                        <td>
+                            <a href="#" class="glyphicon glyphicon-pencil"></a>
+                            <a href="#" class="glyphicon glyphicon-trash"></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>陳大明</td>
+                        <td>test9@test.com.tw</td>
+                        <td>2016-12-02 12:14:28</td>
+                        <td>
+                            <a href="#" class="glyphicon glyphicon-pencil"></a>
+                            <a href="#" class="glyphicon glyphicon-trash"></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>陳明明</td>
+                        <td>test5@test.com.tw</td>
+                        <td>2016-12-03 12:14:28</td>
+                        <td>
+                            <a href="#" class="glyphicon glyphicon-pencil"></a>
+                            <a href="#" class="glyphicon glyphicon-trash"></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>陳明宣</td>
+                        <td>test19@test.com.tw</td>
+                        <td>2016-12-03 14:22:41</td>
+                        <td>
+                            <a href="#" class="glyphicon glyphicon-pencil"></a>
+                            <a href="#" class="glyphicon glyphicon-trash"></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>陳明宗</td>
+                        <td>test23@test.com.tw</td>
+                        <td>2016-12-03 18:30:11</td>
+                        <td>
+                            <a href="#" class="glyphicon glyphicon-pencil"></a>
+                            <a href="#" class="glyphicon glyphicon-trash"></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </body>
     <script src="/lib/jquery-2.1.4.min.js"></script>
     <script src='/lib/bootstrap/dist/js/bootstrap.min.js'></script>
--- resources/views/adm/page/list.blade.php
+++ resources/views/adm/page/list.blade.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>admin news list</title>
+        <title> admin </title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href='/lib/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet' />
@@ -8,20 +8,18 @@
         <link href='/css/admin/header.css' rel='stylesheet' />
         <link href='/css/admin/dashboard-left.css' rel='stylesheet' />
         <link href='/css/admin/dashboard.css' rel='stylesheet' />
-        <link href='/css/admin/news.css' rel='stylesheet' />
     </head>
     <body>
         @include('adm.header')
         @include('adm.dashboard-left')
         <span class="breadcrumbs col-xs-9">
-            新聞列表
-            <a href="#">+</a>
+            頁面列表
         </span>
-        <div class="col-xs-9 newsList">
+        <div class="col-xs-9 memList">
             <table class="col-xs-12 table">
                 <thead>
                     <tr>
-                        <td>新聞標題</td>
+                        <td>頁面標題</td>
                         <td>建立時間</td>
                         <td>修改時間</td>
                         <td>操作</td>
@@ -29,7 +27,7 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td>新聞標題1</td>
+                        <td>關於我們</td>
                         <td>2016-11-21 17:34:49</td>
                         <td>2016-12-03 12:14:28</td>
                         <td>
@@ -38,7 +36,7 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>新聞標題2</td>
+                        <td>公司歷史</td>
                         <td>2016-10-14 09:42:28</td>
                         <td>2016-12-02 18:19:38</td>
                         <td>
@@ -47,7 +45,7 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>新聞標題3</td>
+                        <td>聯絡資訊</td>
                         <td>2016-01-23 10:12:01</td>
                         <td>2016-10-01 14:14:28</td>
                         <td>
@@ -56,7 +54,7 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>新聞標題4</td>
+                        <td>加入我們</td>
                         <td>2015-07-22 00:12:33</td>
                         <td>2016-11-16 10:30:41</td>
                         <td>
@@ -65,7 +63,7 @@
                         </td>
                     </tr>
                     <tr>
-                        <td>新聞標題5</td>
+                        <td>如何合作</td>
                         <td>2016-04-28 09:44:34</td>
                         <td>2016-05-12 08:18:59</td>
                         <td>
