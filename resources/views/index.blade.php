7442b9a20ce5c4981d430b1fbdd9314836ef931a
test <test@test0047.axcell28.idv.tw>
2017-02-18 17:00:57 +0800

    header move to header.blade.php, redesign topmenu css, one page h2 css design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,24 +11,7 @@
         <link href='/css/index.css' rel='stylesheet' />
     </head>
     <body>
-        <nav class="navbar header col-xs-12 col-sm-12 col-md-12">
-            <div class="container-fluid">
-                <button type="button" class="navbar-toggle glyphicon glyphicon-option-horizontal" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                </button>
-                <a class="navbar-brand" href="index.php">
-                    <img src="imgs/absLogo.png" class=""/>
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav topmenu">
-                    <li><a href="/index.php/onepage/1">關於我們</a></li>
-                    <li><a href="/index.php/news">最新消息</a></li>
-                    <li><a href="/index.php/memberLogin">會員專區</a></li>
-                    <li><a href="/index.php/contact">聯絡我們</a></li>
-                </ul>
-            </div>
-        </nav>
+        @include('header')
         <div class="ad col-xs-12 col-sm-12 col-md-12">
             <ul class="bgSlider col-xs-12 ">
                 <li><img src="http://bxslider.com/images/730_200/tree_root.jpg" /></li>
--- resources/views/page/one.blade.php
+++ resources/views/page/one.blade.php
@@ -6,30 +6,14 @@
         <link href='/lib/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet' />
         <link href='/lib/bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet' />
         <link href='/css/header.css' rel='stylesheet' />
+        <link href='/css/one.css' rel='stylesheet' />
     </head>
     <body>
-        <nav class="navbar header col-xs-12 col-sm-12 col-md-12">
-            <div class="container-fluid">
-                <button type="button" class="navbar-toggle glyphicon glyphicon-option-horizontal" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                </button>
-                <a class="navbar-brand" href="index.php">
-                    <img src="/imgs/absLogo.png" class=""/>
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav topmenu">
-                    <li><a href="/index.php/onepage/1">關於我們</a></li>
-                    <li><a href="/index.php/news">最新消息</a></li>
-                    <li><a href="/index.php/memberLogin">會員專區</a></li>
-                    <li><a href="/index.php/contact">聯絡我們</a></li>
-                </ul>
-            </div>
-        </nav>
+        @include('header')
         <div class="oneContent col-xs-12 col-sm-9 col-md-9">
             <h2 class="col-xs-12">{{$title}}</h2>
             <div class="col-xs-12">
-                {{$content}}
+                {!! $content !!}
             </div>
         </div>
         <div class="oneNav col-xs-12 col-sm-3 col-md-3">
