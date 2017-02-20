518ad0c907c6a592c581dfcdef1305af72871e32
test <test@test0047.axcell28.idv.tw>
2017-02-20 18:11:50 +0800

    one page right css design

--- resources/views/page/one.blade.php
+++ resources/views/page/one.blade.php
@@ -18,9 +18,18 @@
         </div>
         <div class="oneNav col-xs-12 col-sm-3 col-md-3">
             <h4 class="col-xs-12">其他介紹</h4>
-            <a href="2" class="col-xs-12"> 公司歷史 </a>
-            <a href="3" class="col-xs-12"> 公司文化 </a>
-            <a href="4" class="col-xs-12"> 人才招募 </a>
+            <p class="col-xs-12">
+                <label class="col-xs-1">1</label>
+                <a href="2" class="col-xs-9"> 公司歷史 </a>
+            </p>
+            <p class="col-xs-12">
+                <label class="col-xs-1">2</label>
+                <a href="3" class="col-xs-9"> 公司文化 </a>
+            </p>
+            <p class="col-xs-12">
+                <label class="col-xs-1">3</label>
+                <a href="4" class="col-xs-9"> 人才招募 </a>
+            </p>
         </div>
     </body>
     <script src="/lib/jquery-2.1.4.min.js"></script>
