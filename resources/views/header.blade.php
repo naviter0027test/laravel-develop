e49c14a27d723d353a72af6171a5bf9eeebce092
user <user@test0059.axcell28.idv.tw>
2018-10-11 09:59:03 +0800

    use google translate tool

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -36,10 +36,21 @@
                 <label class="glyphicon glyphicon-envelope"></label>
                 <a href="/contact">{{ trans('header.contact') }}</a>
             </li>
+<!--
             <li>
                 <label class="glyphicon glyphicon-globe"></label>
                 <a href="/chooseLan">{{ trans('header.choose_lan') }}</a>
             </li>
+-->
+
+<!-- google 翻譯小工具 -->
+<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'zh-TW', includedLanguages: 'en,ja,zh-CN,zh-TW', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<!-- google 翻譯小工具 -->
+
         </ul>
     </div>
 </nav>
